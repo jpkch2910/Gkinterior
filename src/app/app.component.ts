1b2aee4c17fc0c80055604d2d01538159f7c8c10
H, Jayaprakash C <jayaprakash_c_h@dell.com>
2021-06-01 11:32:49 +0530

    changes1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'gkinterior';
+   images:string[]=new Array(41)
+   
+   ngOnInit(){
+
+  this.images[0]="photo"
+  for(var i = 1;i<41;i++){
+    this.images[i]="photo"+i
+  }
+  
+}
 }
